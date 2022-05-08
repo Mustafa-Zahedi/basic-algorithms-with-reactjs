b65ff603a3266b8b95fbd3a562e200e663c28701
Mustafa-Zahedi <malestani21@gmail.com>
2022-05-08 11:40:41 +0430

    chats.jsx added

--- pages/charts.jsx
+++ pages/charts.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
 
 const Charts = () => {
-  setInterval(() => {
-    console.log(new Date());
-  }, 1000);
+  const span = <span className={`bg-slate-600 w-10 h-32`}>10</span>;
 
   useEffect(() => {
     console.log("useEffect");
@@ -11,9 +9,9 @@ const Charts = () => {
 
   return (
     <div className="flex gap-2 items-end">
-      <span className={`bg-slate-600 w-10 h-${20}`}>20</span>
-      <span className={`bg-slate-600 w-10 h-${24}`}>24</span>
-      <span className={`bg-slate-600 w-10 h-${10}`}>10</span>
+      <span className={`bg-slate-600 w-10 h-20`}>20</span>
+      <span className={`bg-slate-600 w-10 h-52`}>16</span>
+      {span}
     </div>
   );
 };
